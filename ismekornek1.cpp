b344079483bb07b8102beceb8394b1de8f5c287f
Emrcnkc <161652013+Emrcnkc@users.noreply.github.com>
2024-03-17 05:32:21 +0300

    Add files via upload

---
+++ ismekornek1.cpp
@@ -0,0 +1,113 @@
+﻿
+ #include <iostream>    
+ #include <string>
+#include <math.h>
+using namespace std;
+string metin = "İSMEK DENEME KODLAR CPP";
+string adsoyad = "EMRE CANAKCI";
+string egitim="C#  PROGRAMLAMA";
+string w;
+ int sayi1 = 90, x = 25, sonuc  ;
+double pi = 3.14;
+char s = 'd';
+float y;
+int u;
+int p;
+int sayyi = 0, fak = 1;
+ int main()
+{   // dünyayı vurguladık
+   // cout << "MERHABA \"DUNYA\" \n merhaba Dunya2"<<endl;
+ // cout << "MERHABA DUNYA"<<endl;
+ // cout << "MERHABA DUNYA2";
+  //  sonuc = sayi1 + x;
+    //cout << "SONUC: " << sonuc;   
+     //cout << metin;
+   //  cout << 15 % 3 << endl;
+    // cout << metin.length(); // size la length aynı komut değerin uzunluğunu verir
+    // cout << metin.size();
+    // cout << adsoyad[0] << adsoyad[1] << adsoyad[2]<< adsoyad[3]
+      //    << adsoyad[4] << adsoyad[5] << adsoyad[6] << adsoyad[7] << adsoyad[8] << adsoyad[9] << adsoyad[10]<<adsoyad[11];
+     //cout << egitim[1];
+     //egitim[1] = '+';
+     //egitim[2] = '+';
+    // cout << egitim;
+    // getline(cin, w);
+    // cin >> w;
+  //   cout << "KLAVYEDEN GIRILEN DEGER=" << w;
+    // cout << "KLAVYEDEN BIR SAYI GIRINIZ: ";
+    // cin >> y;
+     //cout <<"girilen sayinin sinusu"<< sin(y)<<endl;
+    // cout << "girilen sayinin cosu" << cos(y) << endl;
+     //cout << "girilen sayinin karkoku" << sqrt(y);
+    // cin >> u;
+    // if (u>=5)
+    // {
+    //     cout << "MERHABA";
+    // }
+    // else if(u<=3)
+    // {
+    //     cout << "ismek";
+    // }
+    // else
+    // {
+     //    cout << "ibb";
+    // }
+   /*  cin >> u;
+     switch (u)
+     {
+     case 1:
+         cout << "pazartesi";
+          break;
+
+     case 2:
+         cout << "sali";
+         break;
+
+     case 3:
+         cout << "carsamba";
+         break;
+
+     case 4:
+         cout << "persembe";
+         break;
+
+     case 5:
+         cout << "cuma";
+         break;
+
+     case 6:
+         cout << "cumartesi";
+         break;
+
+     case 7:
+         cout << "pazar";
+         break;
+         
+     default :
+             cout << "girilen deger yanlis";
+         break;
+     }*/
+   /* for (p = 0; p <= 50; p += 5)
+     {
+         cout << p << endl; // Her adımda 'p' değerini yazdırıyoruz
+         
+         if (p == 50) // 'p' değeri 50'ye geldiğinde
+         {
+             cout << " "<<endl;
+               cout << "50'ye ulaştınız."<<  endl; // Bu mesajı yazdırıyoruz
+         }
+     
+       
+     } 
+  
+     return 0;*/ 
+   /*cin >> sayyi;
+     for (int i = 1; i <= sayyi; i++)
+     {
+         fak *= i;
+     }cout << fak<<endl;
+     return 0;
+     */
+}
+
+ 
